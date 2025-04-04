0f9e179fe4b20c4f961ee7568781ec9e059a73dc
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-04-04 19:56:15 +0530

    vercel

--- xtaze-backend/src/framework/web/app.ts
+++ xtaze-backend/src/framework/web/app.ts
@@ -15,10 +15,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'https://www.xtaze.fun',
-    credentials: true,                  
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: '*'
   }));
   
 app.use('/webhook', stripeRoute)
