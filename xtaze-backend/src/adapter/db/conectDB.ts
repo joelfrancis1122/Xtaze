12394d04319ee5d5e0e7a57961d7a206b8f1f0d7
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-04-05 00:46:32 +0530

    error fixing

--- xtaze-backend/src/adapter/db/conectDB.ts
+++ xtaze-backend/src/adapter/db/conectDB.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://joelfrancis1122:Joel@2005@xtaze.kbgsujo.mongodb.net/');
+    await mongoose.connect('mongodb+srv://joelfrancis1122:Joel%402005@xtaze.kbgsujo.mongodb.net/');
     console.log('MongoDB Connected!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
