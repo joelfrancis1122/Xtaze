6885d98c56d7ac80a352aa40700ea193b25302c2
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-04-04 21:05:21 +0530

    cors

--- xtaze-backend/src/framework/web/app.ts
+++ xtaze-backend/src/framework/web/app.ts
@@ -1,23 +1,33 @@
 import express from "express";
 import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
-import route from "../routes/index"
-import stripeRoute from "../routes/webhook"
+import route from "../routes/index";
+import stripeRoute from "../routes/webhook";
 import errorMiddleware from "../middlewares/errorMiddleware";
 import notFoundHandler from "../middlewares/notFoundMiiddleware";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 import { setupCronJobs } from "../routes/cron/cronJobs";
+
 dotenv.config();
 
 const app = express();
-const corsOptions:CorsOptions = {origin:["https://xtaze.fun/"],credentials:true}
+
+const corsOptions: CorsOptions = {
+  origin: ["https://xtaze.fun"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
 
-app.use('/webhook', stripeRoute)
+app.use("/webhook", stripeRoute);
 app.use(express.json());
-app.use(cookieParser())
-app.use('/', route)
+app.use(cookieParser());
+app.use("/", route);
 app.use(notFoundHandler);
 app.use(errorMiddleware);
-setupCronJobs()
-export default app
+setupCronJobs();
+
+export default app;
