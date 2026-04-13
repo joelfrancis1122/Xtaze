bc3244be313fbda45149b5b2a68f6d57467cf6f9
joelfrancis1122 <joelfrancis9398@gmail.com>
2026-04-13 16:22:31 +0530

    fix: homepage glitch fixed

--- xtaze-frontend/src/pages/User/UserPrivateRoute.tsx
+++ xtaze-frontend/src/pages/User/UserPrivateRoute.tsx
@@ -2,17 +2,17 @@ import { useSelector } from "react-redux";
 import {Outlet, useNavigate } from "react-router-dom";
 import { RootState } from "../../store/store";
 import { useClearAuth } from "../../utils/useClearAuth";
-
+import { Navigate } from "react-router-dom";
 const PrivateRoute = () => {
   const clearAuth = useClearAuth();
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const role = useSelector((state: RootState) => state.user.signupData?.role);
   const user = useSelector((state: RootState) => state.user.signupData);
 
   if (role !== "user" || !user?.isActive) {
     clearAuth(); 
-    navigate("/")
-    return null; 
+    // navigate("/")
+    return <Navigate to="/" replace />; // No crash, no black screen
   }
 
   return <Outlet />;
