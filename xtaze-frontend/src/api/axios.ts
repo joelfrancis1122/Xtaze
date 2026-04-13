79ee85822c59a04e6aa8c49876587af2ee264719
joelfrancis1122 <joelfrancis9398@gmail.com>
2026-04-13 13:27:57 +0530

    fix

--- xtaze-frontend/src/api/axios.ts
+++ xtaze-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBaseConfig = {
-  baseURL: import.meta.env.VITE_BASE_URL || "https://api.xtaze.fun",
+  baseURL: import.meta.env.VITE_BASE_URL || "https://api.xtaze.in",
   timeout: 50000,
 };
 
